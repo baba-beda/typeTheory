1b2050a16f44afea8b9c73b37fa8c95c0324fb26
Daria Zenkova <darya.nightmare@gmail.com>
2016-09-14 12:32:26 +0300

    files for project added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,22 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Task4.solve();
+        for (String arg : args) {
+            int n = Integer.parseInt(arg);
+            switch (n) {
+                case 1:
+                    Task1.solve();
+                    break;
+                case 2:
+                    Task2.solve();
+                    break;
+                case 3:
+                    Task3.solve();
+                    break;
+                case 4:
+                    Task4.solve();
+                    break;
+            }
+        }
     }
 }
